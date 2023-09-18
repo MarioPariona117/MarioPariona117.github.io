0802b6d324c5c94bdae9c71ce74f0be631a025fb
Mario Pariona <mariopariona117@gmail.com>
2023-09-18 16:33:01 -0500

    Updates

---
+++ precache-manifest.0e519f718503c5ccce3f547eb6dc214b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0865c900c3d98107c9fa",
+    "url": "/static/css/main.46443b34.chunk.css"
+  },
+  {
+    "revision": "0865c900c3d98107c9fa",
+    "url": "/static/js/main.95c4c321.chunk.js"
+  },
+  {
+    "revision": "44379f9d8a742a556af9",
+    "url": "/static/js/runtime~main.1b973b83.js"
+  },
+  {
+    "revision": "23e3074eb60f37242ea4",
+    "url": "/static/js/2.f4b17019.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "7be5df48e3343125beabf65956f9e00c",
+    "url": "/index.html"
+  }
+];
