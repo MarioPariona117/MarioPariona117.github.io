9e844f7a89169c4e2c276f5d0ac3697084f4b2e5
Mario Pariona Molocho <mariopariona117@gmail.com>
2024-02-16 17:37:23 +0000

    Updates

---
+++ precache-manifest.a295da8cd9f795ec4af2b691ea8599f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436a960a865a42714855efa543b0e934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb888c953569588c69e5",
+    "url": "/static/css/main.fa72a2f1.chunk.css"
+  },
+  {
+    "revision": "a7d39cdeb85869282fc4",
+    "url": "/static/js/2.444fa182.chunk.js"
+  },
+  {
+    "revision": "9417166b9fd15ad1ab18",
+    "url": "/static/js/3.69c2b8f0.chunk.js"
+  },
+  {
+    "revision": "eb888c953569588c69e5",
+    "url": "/static/js/main.2708ae2d.chunk.js"
+  },
+  {
+    "revision": "60239c057f5ac77158a7",
+    "url": "/static/js/runtime~main.efc0c561.js"
+  },
+  {
+    "revision": "f98cd910425bf727bd54ce767a9b6884",
+    "url": "/static/media/PressStart2P-Regular.f98cd910.ttf"
+  },
+  {
+    "revision": "e7789f2202c55ee4373f072945cf6a24",
+    "url": "/static/media/RobotoSlab-Regular.e7789f22.ttf"
+  },
+  {
+    "revision": "7542a013c234d9cdbab51d9a712206f9",
+    "url": "/static/media/RobotoSlab-VariableFont_wght.7542a013.ttf"
+  },
+  {
+    "revision": "c8ca9c5cab2861cf95fc328900e6f1a3",
+    "url": "/static/media/UbuntuMono-Regular.c8ca9c5c.ttf"
+  },
+  {
+    "revision": "8a7f6c653add6faea8b1c766ef646b40",
+    "url": "/static/media/background.8a7f6c65.png"
+  }
+]);
